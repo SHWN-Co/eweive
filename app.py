3c3df44cdc9605fff527c0c8a33e5d7b13ad669c
Siema Alam <siemaalam@gmail.com>
2022-12-02 16:52:06 -0500

    Add flask boiler plate, basic file system

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask import render_template # (once we start creating html pages)
+from datetime import datetime
+
+app = Flask(__name__)
+
+# use python -m flask to run the app in VSCode
+@app.route("/")
+def home():
+    return "Hello, Flask!"
+
+@app.route("/homepage/")
+@app.route("/homepage/<name>") 
+def hello_there(name = None):
+    return render_template(
+        "homepage.html",
+        name=name,
+        date=datetime.now()
+    )
+
