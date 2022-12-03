46d2bf3623c8d81f053ca0c2393bb0a3bf9b8f15
Hong Wei Chen <hongwei420@gmail.com>
2022-12-03 16:09:01 -0500

    create __init__.py
    created file for creating database and modifying database

---
+++ __init__.py
@@ -0,0 +1,28 @@
+import sqlite3
+
+file = 'eweive.db'
+
+def db_ex(cmd):
+    db = sqlite3.connect(file)
+    c = db.cursor()
+    out = c.execute(cmd)
+    db.commit()
+    return out
+
+def db_setup():
+    db_ex("DROP TABLE IF EXISTS \"user\"")
+    db_ex("DROP TABLE IF EXISTS \"transcations\"")
+    db_ex("""CREATE TABLE IF NOT EXISTS  'user'(
+        id INTEGER PRIMARY KEY NOT NULL,
+        username VARCHAR,
+        password VARCHAR,
+        phone_number VARCHAR,
+        cc_number VARCHAR,
+        email VARCHAR,);""")
+    db_ex("""CREATE TABLE IF NOT EXISTS 'transaction'(
+        id INTEGER PRIMARY KEY NOT NULL,
+        transaction_number VARCHAR,
+        buyer_id VARCHAR,
+        seller_id, VARCHAR
+        price VARCHAR,
+        time TIMESTAMP,);""")
