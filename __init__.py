5167dd9f684da81708d0ebff9dc8a29a04b6c4d0
Noel Mathew <noelsmathew@gmail.com>
2022-12-03 23:20:55 -0500

    update init

--- __init__.py
+++ __init__.py
@@ -13,7 +13,7 @@ def db_ex(cmd):
 def db_setup():
     db_ex("DROP TABLE IF EXISTS USERS")
     db_ex("""CREATE TABLE IF NOT EXISTS USERS (
-            id INTEGER PRIMARY KEY NOT NULL, 
+            id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
             username TEXT NOT NULL UNIQUE,
             password TEXT NOT NULL,
             user_type TEXT NOT NULL,
@@ -34,8 +34,9 @@ def db_setup():
        
         FOREIGN KEY(buyer_id) REFERENCES USERS (id),
         FOREIGN KEY(seller_id) REFERENCES USERS (id)
+
        ); """)
-#figure out why foreign key is running into trouble ^
+
     db_ex("DROP TABLE IF EXISTS OU_APPLICATION")    
     db_ex("""CREATE TABLE IF NOT EXISTS OU_APPLICATIONS (
         id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
@@ -48,25 +49,27 @@ def db_setup():
         password VARCHAR(255) NOT NULL,
         annual_income INTEGER NOT NULL
        ); """)  
-     
+
+  
+
 
     # create first superuser
     db_ex('''
-        INSERT INTO USERS(id, username, password, user_type, email) 
-        VALUES('8685', 'shwn', '173897', 'SU', 'sudevs@gmail.com')
+        INSERT INTO USERS(username, password, user_type, email) 
+        VALUES('shwn', '173897', 'SU', 'sudevs@gmail.com')
         ''')
     
     # add first item transaction
-    db_ex('''
+db_ex('''
     INSERT INTO TRANSACTIONS(date_and_time, item_id, buyer_id, seller_id, highest_bid)
     VALUES('2022-12-03 12:45:34', '5556', '8790', '3345', '56')
     ''')
-    db_ex('''
+db_ex('''
     INSERT INTO TRANSACTIONS(date_and_time, item_id, buyer_id, seller_id, highest_bid)
     VALUES('2022-12-04 1:30:09', '8834', '7329', '1123', '310')
     ''')
 
-    db_ex('''
+db_ex('''
     INSERT INTO OU_APPLICATIONS(status, app_id, user_type, first_name, last_name, email, password, annual_income)
     VALUES('pending', '112', 'GUEST USER', 'JIE', 'WIE', 'jwei@cuny.com','secur!ty', '1000000')
     ''')
