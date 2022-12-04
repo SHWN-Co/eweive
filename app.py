d48f886a6818e36143506760bbde1ed57246749b
Hong Wei Chen <hongwei420@gmail.com>
2022-12-03 23:38:56 -0500

    added skeleton for login and signup
    yeah

--- app.py
+++ app.py
@@ -1,7 +1,39 @@
-from flask import Flask, render_template, request, url_for, redirect, session # (once we start creating html pages)
+from flask import Flask, render_template, request, url_for, redirect, session, flash # (once we start creating html pages)
+from flask_bootstrap import Bootstrap
+from flask_login import LoginManager, UserMixin
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
 from datetime import datetime
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'bruhmoment'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///eweive.db'
+Bootstrap(app)
+db = SQLAlchemy(app)
+ 
+class User(db.Model, UserMixin):
+    __tablename__ = 'USERS'
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(20), unique = True, nullable = False)
+    password = db.Column(db.String(80), nullable = False)
+    user_type = db.Column(db.String(10), nullable = False)
+    phone_number = db.Column(db.String(100))
+    cc_number = db.Column(db.String(100))
+    email = db.Column(db.String(50), nullable = False, unique = True)
+    def get_id(self):
+        return self.id
+ 
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=20)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=6, max=80)])
+ 
+class RegisterForm(FlaskForm):
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=20)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=6, max=80)])
+    email = StringField('email', validators=[InputRequired(), Length(min = 8, max = 80)])
+
 
 # use python -m flask to run the app in VSCode
 @app.route("/", methods=['GET','POST'])
@@ -11,6 +43,16 @@ def home():
 @app.route('/OUApplication', methods = ['GET', 'POST'])
 def OUApplication():
     return render_template("OUApplication.html")
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return "login"
+ 
+@app.route("/signup")
+def signup():
+    form = RegisterForm()
+    return "signup"
 
 @app.route("/homepage/<name>") 
 def hello_there(name = None):
