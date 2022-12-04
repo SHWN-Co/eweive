0c1c2476746bb48799b2a07f9c7928592af9d7db
Siema Alam <siemaalam@gmail.com>
2022-12-03 23:30:14 -0500

    create homepage html and nav to ou

--- app.py
+++ app.py
@@ -4,9 +4,13 @@ from datetime import datetime
 app = Flask(__name__)
 
 # use python -m flask to run the app in VSCode
-@app.route("/")
+@app.route("/", methods=['GET','POST'])
 def home():
     return render_template("homepage.html")
+
+@app.route('/OUApplication', methods = ['GET', 'POST'])
+def OUApplication():
+    return render_template("OUApplication.html")
 
 @app.route("/homepage/<name>") 
 def hello_there(name = None):
@@ -15,4 +19,7 @@ def hello_there(name = None):
         name=name,
         date=datetime.now()
     )
+
+
+
 
