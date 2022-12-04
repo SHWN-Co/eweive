9c18669bd2fe29620b6b157c438a8efc2e349aa9
Siema Alam <siemaalam@gmail.com>
2022-12-03 19:06:50 -0500

    Update DB and setup

--- __init__.py
+++ __init__.py
@@ -7,22 +7,23 @@ def db_ex(cmd):
     c = db.cursor()
     out = c.execute(cmd)
     db.commit()
+    db.close()
     return out
 
 def db_setup():
-    db_ex("DROP TABLE IF EXISTS \"user\"")
-    db_ex("DROP TABLE IF EXISTS \"transaction\"")
-    db_ex("""CREATE TABLE IF NOT EXISTS  'user'(
-        id INTEGER PRIMARY KEY NOT NULL,
-        username VARCHAR,
-        password VARCHAR,
-        phone_number VARCHAR,
-        cc_number VARCHAR,
-        email VARCHAR);""")
-    db_ex("""CREATE TABLE IF NOT EXISTS 'transaction'(
-        id INTEGER PRIMARY KEY NOT NULL,
-        transaction_number VARCHAR,
-        buyer_id VARCHAR,
-        seller_id, VARCHAR
-        price VARCHAR,
-        time TIMESTAMP);""")
+    db_ex("DROP TABLE IF EXISTS USERS")
+    db_ex("""CREATE TABLE IF NOT EXISTS USERS (
+            id INTEGER PRIMARY KEY NOT NULL, 
+            username TEXT NOT NULL UNIQUE,
+            password TEXT NOT NULL,
+            user_type TEXT NOT NULL,
+            phone_number TEXT,
+            cc_number TEXT,
+            email TEXT NOT NULL); """)
+    # create first superuser
+    db_ex('''
+        INSERT INTO USERS(id, username, password, user_type, email) 
+        VALUES('8685', 'Shwn', '173897', 'SU', 'sudevs@gmail.com')
+        ''')
+
+db_setup()
